559ffd59fbc1cb74183b1cabe5d6c9c62066455b
sinnedpenguin <sinnedpenguin@proton.me>
2023-10-19 23:45:16 +0800

    feat: Vercel web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 import SiteFooter from "@/components/site-footer"
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: {
@@ -46,6 +47,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <div className="relative flex min-h-screen flex-col">
               <SiteHeader />
               <div className="flex-1">{children}</div>
+              <Analytics />
               <Toaster />
               <SiteFooter />
             </div>
