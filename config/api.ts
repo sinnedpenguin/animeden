04795027e53a5d1c7dbb097f897d812ca24f0eb4
sinnedpenguin <sinnedpenguin@proton.me>
2023-10-21 00:20:42 +0800

    fix: Ensure consistent URL construction in EP path

--- config/api.ts
+++ config/api.ts
@@ -1,7 +1,12 @@
 export const paths = {
   search: (query: string) => `${process.env.NEXT_PUBLIC_SEARCH_API_PATH}${encodeURIComponent(query)}`,
   info: (id: string) => `${process.env.NEXT_PUBLIC_INFO_API_PATH}${id}`,
-  episode: (episodeId: string) => `${process.env.NEXT_PUBLIC_EPISODE_API_PATH}${episodeId}`,
+
+  episode: (episodeId: string) => {
+    const episodePath = episodeId.startsWith('/') ? episodeId : `/${episodeId}`;
+    return `${process.env.NEXT_PUBLIC_EPISODE_API_PATH}${episodePath}`;
+  },
+  
   trending: process.env.NEXT_PUBLIC_TRENDING_API_PATH,
   popular: process.env.NEXT_PUBLIC_POPULAR_API_PATH,
   random: process.env.NEXT_PUBLIC_RANDOM_API_PATH,
